2ccde1732251da2154d58a180540ce94f0457b94
t94jgj9yn9-dotcom <t94jgj9yn9@privaterelay.appleid.com>
2026-05-28 13:24:45 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'gewicht-pwa-v1';
+const LOCAL = ['./index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(LOCAL).catch(() => {})));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Don't cache CDN or external requests
+  if (!e.request.url.startsWith(self.location.origin)) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
